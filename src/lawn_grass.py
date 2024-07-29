56c68d1967f7b5de8cc3af5e0dcf18722b855ae7
Timofeev Artem <artemtimofeev@rambler.ru>
2024-07-29 17:59:56 +0300

    Некоторые правки.

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -2,6 +2,8 @@ from src.product import Product
 
 
 class LawnGrass(Product):
+    """Класс описывает газонную траву. Родительский класс - Product"""
+
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         self.country = country
         self.germination_period = germination_period
