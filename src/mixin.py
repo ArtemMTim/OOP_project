411210263ad3748a5c7cc695bbde50642fa391b7
Timofeev Artem <artemtimofeev@rambler.ru>
2024-08-01 09:56:55 +0300

    Некоторые правки.

--- src/mixin.py
+++ src/mixin.py
@@ -3,4 +3,7 @@ class MixinLog:
     и с какими параметрами создан объект."""
 
     def __init__(self):
+        self.__repr__()
+
+    def __repr__(self):
         print(f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})")
